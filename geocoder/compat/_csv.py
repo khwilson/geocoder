4d7eb813db89128ce9c9c2950c40d9df67fcc7bb
Kevin Wilson <khwilson@gmail.com>
2016-07-05 13:22:52 -0500

    Allow for csv compatibilizer to write to stdout

--- geocoder/compat/_csv.py
+++ geocoder/compat/_csv.py
@@ -25,18 +25,14 @@ class UnicodeCsvReader:
         if PY3:
             if self.filename.endswith('.gz'):
                 self.f = gzip.open(self.filename, 'rt')
-                #reader = codecs.getreader('utf-8')(self.f)
-                #self.reader = csv.reader(reader)
             else:
-                self.f = open(self.filename, 'rt',
-                              encoding=self.encoding, newline='')
+                self.f = open(self.filename, 'rt', encoding=self.encoding, newline='')
         else:
             if self.filename.endswith('.gz'):
                 self.f = gzip.open(self.filename)
             else:
                 self.f = open(self.filename, 'rb')
-        self.reader = csv.reader(self.f, dialect=self.dialect,
-                                 **self.kw)
+        self.reader = csv.reader(self.f, dialect=self.dialect, **self.kw)
         return self
 
     def __exit__(self, type, value, traceback):
@@ -58,19 +54,18 @@ class UnicodeCsvReader:
 
 
 class UnicodeCsvWriter:
-    def __init__(self, filename, dialect=csv.excel,
-                 encoding="utf-8", **kw):
+    def __init__(self, filename, dialect=csv.excel, encoding="utf-8", **kw):
         self.filename = filename
         self.dialect = dialect
         self.encoding = encoding
         self.kw = kw
 
     def __enter__(self):
-        if PY3:
+        if not self.filename:
+            self.f = sys.stdout
+        elif PY3:
             if self.filename.endswith('.gz'):
                 self.f = gzip.open(self.filename, 'wt')
-                #writer = codecs.getwriter('utf-8')(self.f)
-                #self.writer = csv.writer(writer, dialect=self.dialect, **self.kw)
             else:
                 self.f = open(self.filename, 'wt', encoding=self.encoding, newline='')
         else:
